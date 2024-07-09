3e639bc554e4c333d4034029ab3d8cd23eaf59db
Yuriy Dupyn <2153100+omedusyo@users.noreply.github.com>
2024-07-09 21:21:43 +0200

    Implement positivity check

--- ast/src/validation/type_checker.rs
+++ ast/src/validation/type_checker.rs
@@ -1,6 +1,6 @@
 use crate::parser::{
     identifier::{Variable, ConstructorName},
-    base::{Program, TypeDeclaration, Type}
+    base::{Program, TypeDeclaration, Type, ConstructorDeclaration}
 };
 
 pub type Result<A> = std::result::Result<A, Error>;
@@ -12,9 +12,14 @@ pub enum Error {
     TypeConstructorDoesntExist { constructor_name: ConstructorName },
     TypeConstructorIsApplliedToWrongNumberOfArguments { expected: usize, received: usize },
     UndefinedTypeVaraible { variable: Variable },
+    NegativeOccuranceOfRecursiveTypeVariableInInductiveDeclaration { variable: Variable }
 }
 
 fn check_type_declaration(program: &Program, decl: &TypeDeclaration) -> Result<()> {
+    todo!()
+}
+
+fn check_constructor_declaration(program: &Program, type_env: &TypeEnvironment, decl: &ConstructorDeclaration) {
     todo!()
 }
 
@@ -39,7 +44,6 @@ fn check_type(program: &Program, type_env: &TypeEnvironment, type_: &Type) -> Re
             }
         },
         TypeApplication(type_constructor_name, types) => {
-            // TODO: Check arity and check each type inside with the same env
             match program.get_type_declaration(type_constructor_name) {
                 Some(decl) => {
                     if decl.arity() == types.len() {
@@ -61,6 +65,55 @@ fn check_type(program: &Program, type_env: &TypeEnvironment, type_: &Type) -> Re
             check_type(program, type_env, &function_type.output_type)
         }
     }
+}
+
+#[derive(Debug, Copy, Clone)]
+enum Polarity {
+    Positive,
+    Negative
+}
+
+impl Polarity {
+    pub fn flip(self) -> Self {
+        use Polarity::*;
+        match self {
+            Positive => Negative,
+            Negative => Positive,
+        }
+    }
+}
+
+fn check_positive_occurance(type_var0: &Variable, type_: &Type) -> Result<()> {
+
+    fn check(type_var0: &Variable, type_: &Type, polarity: Polarity) -> Result<()> {
+        use Type::*;
+        match type_{
+            VariableUse(type_var) => {
+                if type_var == type_var0 {
+                    match polarity {
+                        Polarity::Positive => Ok(()),
+                        Polarity::Negative => Err(Error::NegativeOccuranceOfRecursiveTypeVariableInInductiveDeclaration { variable: type_var.clone() }),
+                    }
+                } else {
+                    Ok(())
+                }
+            },
+            TypeApplication(_, types) => {
+                for type_ in types {
+                    check(type_var0, type_, polarity)?
+                }
+                Ok(())
+            },
+            Arrow(function_type) => {
+                for type_ in &function_type.input_types {
+                    check(type_var0, type_, polarity.flip())?
+                }
+                check(type_var0, &function_type.output_type, polarity)
+            }
+        }
+    }
+
+    check(type_var0, type_, Polarity::Positive)
 }
 
 #[cfg(test)]
