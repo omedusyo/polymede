43becbdfe3ee18d6daded3a7b5d128740ef19803
Yuriy Dupyn <2153100+omedusyo@users.noreply.github.com>
2024-07-27 19:17:46 +0200

    Implement stack inspection tools. Add env tag to environments on stacks. Fix runtime bugs.

--- runtime/src/memory_inspect.js
+++ runtime/src/memory_inspect.js
@@ -1,37 +1,42 @@
+const ENV_TAG = 0;
 const CONST_TAG = 1;
 const ARRAY_TAG = 2;
 const TUPLE_TAG = 3;
 
 const TAGGED_POINTER_BYTE_SIZE = 5;
 
-function Constant(value) {
-  return { type: "Const", value };
+const PARENT_ENV_POINTER_BYTE_SIZE = 4;
+const ENV_COUNTER_BYTE_SIZE = 4;
+const ENV_HEADER_BYTE_SIZE = 1 + PARENT_ENV_POINTER_BYTE_SIZE + ENV_COUNTER_BYTE_SIZE; // 9 bytes, 1 byte for tag, 4 bytes for parent env pointer, 4 bytes for env count.
+
+function Constant(value, address) {
+  return { type: "Const", value, address };
 }
 
-function TaggedPointer(tag, pointer) {
-  return { type: "Pointer", tag, pointer };
+function TaggedPointer(tag, pointer, address) {
+  return { type: "Pointer", tag, pointer, address };
 }
 
-function Tuple(variant, components) {
-  return { type: "Tuple",  variant, components };
+function Tuple(variant, components, address) {
+  return { type: "Tuple",  variant, components, address };
 }
 
-function Env(old_env_pointer, values) {
-  return { type: "Env", old_env_pointer, values };
+function Array(byte_count, bytes, address) {
+  return { type: "Array",  byte_count, bytes, address };
 }
 
-function Stack(values) {
-  return { type: "Stack", values };
+function Env(old_env_pointer, values, address) {
+  return { type: "Env", old_env_pointer, values, address };
 }
 
 function readRawPointer(view, raw_pointer) {
   const tag = view.getInt8(raw_pointer);
   if (tag == CONST_TAG) {
     const value = view.getInt32(raw_pointer + 1, true);
-    return Constant(value);
+    return Constant(value, raw_pointer);
   } else if (tag == TUPLE_TAG) {
     const tuple_pointer = view.getInt32(raw_pointer + 1, true);
-    return TaggedPointer("Tuple", tuple_pointer);
+    return TaggedPointer("Tuple", tuple_pointer, raw_pointer);
   } else if (tag == ARRAY_TAG) {
     throw Error("Arrays not yet implemented");
   } else {
@@ -58,7 +63,7 @@ function readTuple(view, tagged_pointer) {
     components.push(component);
     next_component_raw_pointer += TAGGED_POINTER_BYTE_SIZE;
   }
-  return Tuple(variant, components);
+  return Tuple(variant, components, tuple_pointer);
 }
 module.exports.readTuple = readTuple;
 
@@ -90,9 +95,92 @@ function deepReadTuple(view, tagged_pointer) {
   tuple.components.forEach(component => {
     components.push(deepReadTaggedPointer(view, component));
   });
-  return Tuple(tuple.variant, components);
+  return Tuple(tuple.variant, components, tuple.address);
 }
 module.exports.deepReadTuple = deepReadTuple;
+
+function readStack(view, raw_pointer_start, raw_pointer_end) {
+  const stack = [];
+
+  function readNextValue(raw_pointer) {
+    if (raw_pointer == raw_pointer_end) {
+      // Done
+    } else {
+      const tag = view.getInt8(raw_pointer);
+      if (tag == CONST_TAG) {
+        const value = view.getInt32(raw_pointer + 1, true);
+        stack.push(Constant(value), raw_pointer);
+        readNextValue(raw_pointer + TAGGED_POINTER_BYTE_SIZE);
+      } else if (tag == TUPLE_TAG) {
+        const tuple_pointer = view.getInt32(raw_pointer + 1, true);
+        stack.push(TaggedPointer("Tuple", tuple_pointer, raw_pointer));
+        readNextValue(raw_pointer + TAGGED_POINTER_BYTE_SIZE)
+      } else if (tag == ENV_TAG) {
+        const old_env_pointer = view.getInt32(raw_pointer + 1, true);
+        const count = view.getInt32(raw_pointer + 1 + PARENT_ENV_POINTER_BYTE_SIZE, true);
+        let next_arg_at = raw_pointer + ENV_HEADER_BYTE_SIZE;
+        const values = [];
+        for (let i = 0; i < count; i++) {
+          const arg = readRawPointer(view, next_arg_at);
+          values.push(arg);
+          next_arg_at = next_arg_at + TAGGED_POINTER_BYTE_SIZE;
+        }
+        stack.push(Env(old_env_pointer, values, raw_pointer));
+        readNextValue(raw_pointer + ENV_HEADER_BYTE_SIZE + count * TAGGED_POINTER_BYTE_SIZE);
+      } else if (tag == ARRAY_TAG) {
+        throw Error("Arrays not yet implemented");
+      } else {
+        throw Error(`Unknown value Tag ${tag}`);
+      }
+    }
+  }
+
+  readNextValue(raw_pointer_start);
+  return stack;
+}
+module.exports.readStack = readStack;
+
+function deepReadStack(view, raw_pointer_start, raw_pointer_end) {
+  const stack = [];
+
+  function readNextValue(raw_pointer) {
+    if (raw_pointer == raw_pointer_end) {
+      // Done
+    } else {
+      const tag = view.getInt8(raw_pointer);
+      if (tag == CONST_TAG) {
+        const value = view.getInt32(raw_pointer + 1, true);
+        stack.push(Constant(value, raw_pointer));
+        readNextValue(raw_pointer + TAGGED_POINTER_BYTE_SIZE);
+      } else if (tag == TUPLE_TAG) {
+        const tuple_pointer = view.getInt32(raw_pointer + 1, true);
+        const tagged_pointer = TaggedPointer("Tuple", tuple_pointer);
+        stack.push(deepReadTuple(view, tagged_pointer));
+        readNextValue(raw_pointer + TAGGED_POINTER_BYTE_SIZE)
+      } else if (tag == ENV_TAG) {
+        const old_env_pointer = view.getInt32(raw_pointer + 1, true);
+        const count = view.getInt32(raw_pointer + 1 + PARENT_ENV_POINTER_BYTE_SIZE, true);
+        let next_arg_at = raw_pointer + ENV_HEADER_BYTE_SIZE;
+        const values = [];
+        for (let i = 0; i < count; i++) {
+          const arg = deepReadRawPointer(view, next_arg_at);
+          values.push(arg);
+          next_arg_at = next_arg_at + TAGGED_POINTER_BYTE_SIZE;
+        }
+        stack.push(Env(old_env_pointer, values, raw_pointer));
+        readNextValue(raw_pointer + ENV_HEADER_BYTE_SIZE + count * TAGGED_POINTER_BYTE_SIZE);
+      } else if (tag == ARRAY_TAG) {
+        throw Error("Arrays not yet implemented");
+      } else {
+        throw Error(`Unknown value Tag ${tag}`);
+      }
+    }
+  }
+
+  readNextValue(raw_pointer_start);
+  return stack;
+}
+module.exports.deepReadStack = deepReadStack;
 
 function showValue(x) {
   if (x.type == "Const") {
@@ -103,8 +191,59 @@ function showValue(x) {
       components_str.push(showValue(component));
     });
     return `Tuple[${x.variant}](${components_str.join(', ')})`;
+  } else if (x.type == "Env") {
+    const values_str = [];
+    x.values.forEach(value => {
+      values_str.push(showValue(value));
+    });
+    return `Env[#${x.old_env_pointer}](${values_str.join(", ")})`;
+  } else if (x.type == "Array") {
+    throw Error("Arrays not yet implemented");
   } else {
     throw Error(`Unexpected value type in ${x}`);
   }
 }
 module.exports.showValue = showValue;
+
+function showValueWithAddress(x) {
+  if (x.type == "Const") {
+    return `Const@${x.address}[${x.value}]`;
+  } else if (x.type == "Tuple") {
+    const components_str = [];
+    x.components.forEach(component => {
+      components_str.push(showValueWithAddress(component));
+    });
+    return `Tuple@${x.address}[${x.variant}](${components_str.join(', ')})`;
+  } else if (x.type == "Env") {
+    const values_str = [];
+    x.values.forEach(value => {
+      values_str.push(showValueWithAddress(value));
+    });
+    return `Env@${x.address}[#${x.old_env_pointer}](${values_str.join(", ")})`;
+  } else if (x.type == "Array") {
+    throw Error("Arrays not yet implemented");
+  } else {
+    throw Error(`Unexpected value type in ${x}`);
+  }
+}
+module.exports.showValueWithAddress = showValueWithAddress;
+
+function showStack(stack) {
+  const strings = [];
+  stack.forEach(x => {
+    strings.push(showValue(x));
+  });
+
+  return `Nil <: ${strings.join(' <: ')}`;
+}
+module.exports.showStack = showStack;
+
+function showStackWithAddress(stack) {
+  const strings = [];
+  stack.forEach(x => {
+    strings.push(showValueWithAddress(x));
+  });
+
+  return `Nil <: ${strings.join(' <: ')}`;
+}
+module.exports.showStackWithAddress = showStackWithAddress;
--- runtime/src/run_wasm.js
+++ runtime/src/run_wasm.js
@@ -1,5 +1,5 @@
 const fs = require('node:fs');
-const { showValue, deepReadRawPointer, readRawPointer, readTuple } = require("./memory_inspect.js");
+const { showValue, showValueWithAddress, showStack, showStackWithAddress, deepReadRawPointer, readRawPointer, readTuple, readStack, deepReadStack } = require("./memory_inspect.js");
 
 function run(bytes) {
   const memory = new WebAssembly.Memory({ initial: 2, maximum: 10 });
@@ -18,30 +18,19 @@ function run(bytes) {
     const end = GLOBAL.STACK.valueOf();
 
 
-    //=====
-    const view = new DataView(memory.buffer);
-    // let tagged_pointer = readRawPointer(view, start);
-    // console.log(tagged_pointer);
-    // let tuple0 = readTuple(view, tagged_pointer);
-    // console.log(tuple0);
-    // let tuple1 = readTuple(view, tuple0.components[1]);
-    // console.log(tuple1);
-
-    console.log("+===================+");
-    const val = deepReadRawPointer(view, start);
-    console.log(showValue(val))
-
-    //=====
-
     const values = array.slice(start, end);
-    // values.forEach(v => {
-    //   console.log(v);
-    // });
-    // console.log(`STACK[${start} to ${end}]`, values);
+    values.forEach(v => {
+      console.log(v);
+    });
+    console.log(`STACK[${start} to ${end}]`, values);
   }
 
   WebAssembly.instantiate(bytes, config).then(({ instance }) => {
     const { main, init, stack_start, stack, env, heap, free, frame } = instance.exports;
+
+    const { make_env, make_env_from, make_env_from_closure, drop_env, tuple, partial_apply } = instance.exports;
+    const const_ = instance.exports["const"];
+    const var_ = instance.exports["var"];
 
     GLOBAL.STACK_START = stack_start;
     GLOBAL.STACK = stack;
@@ -50,11 +39,71 @@ function run(bytes) {
     GLOBAL.FREE = free;
     GLOBAL.FRAME = frame;
 
-    console.log("Instantiated succesfully");
-    console.log(main);
-    console.log(main());
+    console.log("Instantiated succesfully.");
 
-    printRawStack();
+    console.log(main);
+    // TODO
+    // console.log("MAIN IS NOT RUNNING!");
+    const view = new DataView(memory.buffer);
+    console.log(showStack(deepReadStack(view, GLOBAL.STACK_START.valueOf(), GLOBAL.STACK.valueOf())));
+
+    make_env(0);
+    main();
+    drop_env();
+    console.log("Main executed succesfully.");
+    console.log(showStack(deepReadStack(view, GLOBAL.STACK_START.valueOf(), GLOBAL.STACK.valueOf())));
+
+    // printRawStack();
+
+    //=====
+    // let tagged_pointer = readRawPointer(view, start);
+    // console.log(tagged_pointer);
+    // let tuple0 = readTuple(view, tagged_pointer);
+    // console.log(tuple0);
+    // let tuple1 = readTuple(view, tuple0.components[1]);
+    // console.log(tuple1);
+
+    console.log("+===================+");
+    // const val = deepReadRawPointer(view, GLOBAL.STACK_START.valueOf());
+    // console.log(showValue(val))
+
+    // const_(16);
+    // const_(250);
+    // make_env(2)
+
+    // const_(16);
+    // const_(250);
+    // partial_apply(888, 2)
+    //
+    // console.log(showStackWithAddress(deepReadStack(view, GLOBAL.STACK_START.valueOf(), GLOBAL.STACK.valueOf())));
+    //
+    // // console.log("What is this?", readRawPointer(view, 2079));
+    //
+    // const_(123);
+    // const_(124);
+    // const_(125);
+    // console.log(showStackWithAddress(deepReadStack(view, GLOBAL.STACK_START.valueOf(), GLOBAL.STACK.valueOf())));
+    // make_env_from_closure(3)
+    // console.log(showStackWithAddress(deepReadStack(view, GLOBAL.STACK_START.valueOf(), GLOBAL.STACK.valueOf())));
+    // console.log(showStackWithAddress(deepReadStack(view, GLOBAL.STACK_START.valueOf(), GLOBAL.STACK.valueOf())));
+    // console.log(readStack(view, GLOBAL.STACK_START.valueOf(), 5))
+
+
+    // ===Test===
+    // console.log(showStackWithAddress(deepReadStack(view, GLOBAL.STACK_START.valueOf(), GLOBAL.STACK.valueOf())));
+    // const_(50)
+    // const_(60)
+    // make_env(2)
+    // var_(1)
+    // var_(0)
+    // console.log(showStackWithAddress(deepReadStack(view, GLOBAL.STACK_START.valueOf(), GLOBAL.STACK.valueOf())));
+
+    // console.log("STACK_START == ", GLOBAL.STACK_START.valueOf());
+    // console.log("STACK == ", GLOBAL.STACK.valueOf());
+    // const stack_val = readStack(view, GLOBAL.STACK_START.valueOf(), GLOBAL.STACK.valueOf());
+    // const stack_val = deepReadStack(view, GLOBAL.STACK_START.valueOf(), GLOBAL.STACK.valueOf());
+    // console.log(showStack(stack_val));
+    //=====
   });
 }
 
